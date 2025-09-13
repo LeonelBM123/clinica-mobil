05d1c34a212c123f2c8fb000a007f70f69e964bf
Leonel Barrios <leonel.barriosmay@gmail.com>
2025-09-13 18:39:40 -0400

    Login funcional

---
+++ lib/data/services/autentificacion.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final storage = FlutterSecureStorage();
+
+Future<Map<String, dynamic>?> login(String username, String password) async {
+  final url = Uri.parse("http://10.0.2.2:8000/api/usuarios/login/");
+
+
+  try {
+    final response = await http.post(
+      url,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "correo": username,
+        "password": password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      // Guardamos el token en secure storage
+      await storage.write(key: "token", value: data["token"]);
+      await storage.write(key: "rol", value: data["rol"]);
+      await storage.write(key: "usuario_id", value: data["usuario_id"].toString());
+
+      print("✅ ${data['message']}");
+      return data; // Devuelve el JSON completo si quieres usarlo en la UI
+    } else {
+      print("❌ Error en login: ${response.body}");
+      return null; // Login fallido
+    }
+  } catch (e) {
+    print("❌ Ocurrió un error: $e");
+    return null;
+  }
+}
--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,13 +1,58 @@
+import 'package:clinica_visionex/data/services/autentificacion.dart' as autentificacion;
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login'),),
-      body: IconButton(onPressed: ()=>{Navigator.pushNamed(context, '/detalle')}, icon: Icon(Icons.admin_panel_settings,color: Colors.blue,)),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            width: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Clinica Visionex'),
+                SizedBox( height: 15),
+
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: "Correo",
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(12),),
+                  ),
+                ),
+                SizedBox( height: 15),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Contraseña",
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(12),),
+                  ),
+                ),
+                TextButton(onPressed: () async {
+                  final email = emailController.text.trim();
+                  final password = passwordController.text.trim();
+                  final result = await autentificacion.login(email, password);
+                  if (result?['rol']=='Administrador') {
+                    Navigator.pushNamed(context, '/detalle');
+                  }
+                }, child: Text('Ingresar')),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
